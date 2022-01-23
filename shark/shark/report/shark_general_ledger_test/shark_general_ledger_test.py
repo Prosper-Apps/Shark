7b21ec9998fb981dc3338633444248ea6b3a098b
umaepoch <umaepoch@consulting.in>
2022-01-23 10:25:31 +0530

    purchase receipt error fixed

--- shark/shark/report/shark_general_ledger_test/shark_general_ledger_test.py
+++ shark/shark/report/shark_general_ledger_test/shark_general_ledger_test.py
@@ -61,7 +61,7 @@ def execute(filters=None):
 			from `tabPurchase Receipt` where name='"""+d.voucher_no+"""'""", as_dict=1)
 			po_number=frappe.db.sql("""select pri.purchase_order 
 			from `tabPurchase Receipt` pr,`tabPurchase Receipt Item` pri 
-			where pr.name=pri.parent and poi.name='"""+d.voucher_no+"""'""", as_dict=1)
+			where pr.name=pri.parent and pr.name='"""+d.voucher_no+"""'""", as_dict=1)
 			d["supplier"] = receipt_data[0].supplier_name
 			d["supplier_invoice_no"] = receipt_data[0].bill_no
 			if receipt_data[0].bill_date is not None:
