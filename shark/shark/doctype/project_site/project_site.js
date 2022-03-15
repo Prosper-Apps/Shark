a0625c7f92c065b56f16a1135405c1106e647094
umaepoch <umaepoch@consulting.in>
2022-03-15 09:54:35 +0530

    site requisition form doctype added

---
+++ shark/shark/doctype/project_site/project_site.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, jyoti and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Project Site', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ shark/shark/doctype/site_requisition_form/site_requisition_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, jyoti and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Site Requisition Form', {
+	// refresh: function(frm) {
+
+	// }
+});
